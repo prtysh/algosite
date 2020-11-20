6e190ccd02199db9d7029961f6567af55924de09
prtysh <prtyshrmn@gmail.com>
2020-11-21 00:00:31 +0530

    added eyes

--- machine.js
+++ machine.js
@@ -1,6 +1,7 @@
 var classifier;
 var video;
 var detections = [];
+var eyeWidth;
 console.log('ml5 version:', ml5.version);
 
 function preload(){
@@ -9,9 +10,9 @@ function preload(){
 }
 
 function setup() {
-  createCanvas(340, 240);
+  createCanvas(640, 480);
   video = createCapture(VIDEO);
-  video.size(340, 240);
+  video.size(640, 480);
   classifier.detect(video, gotDetections);
   // console.log(classifier);
   video.hide();
@@ -22,6 +23,7 @@ function setup() {
 function draw() {
   background(200);
   image(video,0,0);
+  eyeWidth= 0;
   for (let i =0; i < detections.length; i++){
     let obj = detections[i];
     fill(0);
@@ -30,11 +32,15 @@ function draw() {
     // text(detections[i].confidence, 10, 20);
     if(detections[i].label == 'person'){
       let distance = detections[i].confidence * height;
+      eyeWidth = detections[i].confidence * 50;
       // console.log(distance);
       line(0,distance,width,distance);
+    } else {
+      eyeWidth = 0;
     }
     // console.log(detections[i].label);
   }
+  drawEyes();
 }
 // Initialize the Image Classifier method with MobileNet
 
@@ -58,6 +64,30 @@ function gotDetections(error, results){
   classifier.detect(video, gotDetections);
 }
 
+
+function drawEyes(){
+  rectMode(CENTER);
+noStroke();
+fill(30);
+ellipse(width / 2 - 70, height / 2, 110, 110);
+fill(245, 224, 80);
+ellipse(width / 2 - 70, height / 2, 90, 90);
+fill(30);
+ellipse(width / 2 + 70, height / 2, 110, 110);
+fill(245, 224, 80);
+ellipse(width / 2 + 70, height / 2, 90, 90);
+
+fill(0);
+circle(width / 2 - 70, height / 2, eyeWidth + 3);
+fill(255);
+circle(width / 2 - 70, height / 2, eyeWidth);
+
+
+fill(0);
+circle(width / 2 + 70, height / 2, eyeWidth + 3);
+fill(255);
+circle(width / 2 + 70, height / 2, eyeWidth);
+}
 // Make a prediction with a selected image
 // classifier.classify(document.getElementById('image'), (err, results) => {
 //   console.log(results);
